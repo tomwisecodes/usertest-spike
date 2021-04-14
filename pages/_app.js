3b83202a22713360b72f9f30b7604b0bbbdf8b4a
tomwisecodes <79859203+tomwisecodes@users.noreply.github.com>
2021-04-14 13:49:24 +0100

    dsafd

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    window.onUsersnapCXLoad = function (api) {
+      api.init();
+    };
+    var script = document.createElement("script");
+    script.defer = 1;
+    script.src =
+      "https://widget.usersnap.com/global/load/5b82eb0e-ab1d-4866-b2e7-da95ecb512ab?onload=onUsersnapCXLoad";
+    document.getElementsByTagName("head")[0].appendChild(script);
+    return () => {
+      document.getElementsByTagName("head")[0].removeChild(script);
+    };
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
